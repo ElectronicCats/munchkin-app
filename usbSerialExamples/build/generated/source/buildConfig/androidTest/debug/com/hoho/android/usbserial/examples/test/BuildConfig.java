c7bd75767493f62bcb9adbc1a231f8ef21cd359c
Sabas <s@electroniccats.com>
2025-08-06 14:25:57 -0600

    feat: add project

---
+++ usbSerialExamples/build/generated/source/buildConfig/androidTest/debug/com/hoho/android/usbserial/examples/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.hoho.android.usbserial.examples.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.hoho.android.usbserial.examples.test";
+  public static final String BUILD_TYPE = "debug";
+}
