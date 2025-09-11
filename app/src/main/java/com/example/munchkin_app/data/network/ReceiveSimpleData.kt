c97b1ecdd953dce7acefc951e88dd6897604757f
Mane <emmanuelelegantini@gmail.com>
2025-09-11 17:04:21 -0600

    Usb Helper Protobuff Base

---
+++ app/src/main/java/com/example/munchkin_app/data/network/ReceiveSimpleData.kt
@@ -0,0 +1,9 @@
+package com.example.munchkin_app.data.network
+
+import simple.SimpleMessageKt
+import simple.simpleMessage
+import simple.Simple
+
+fun main() {
+    println(SimpleMessageKt.toString())
+}
